51ca04547d3cf1a72c853422b5050fd6b7542234
pugal26 <pugalcud@gmail.com>
2024-04-24 02:30:47 +0530

    shopping cart completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,18 @@
+//Home.jsx
 import { useState } from "react"
 import data from "../assets/products.json"
 import { Product } from "./Product";
+import '../components/Home.css'
 
 // console.log(data);
 export const Home = ({ cart, setCart }) => {
   const [products] = useState(data);
 
   return (
-   
-            <div className="row"> 
-              <div className="product-container">
-
-      {products.map((product) => (
-        <Product key={product.id} product={product} cart={cart} setCart={setCart}/>
-      ))}
-    </div>
-    </div>
+      <div className="product-container">
+        {products.map((product) => (  
+            <Product product={product} key={product.id} cart={cart} setCart={setCart}/>
+        ))}
+      </div>
   );
 };
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,7 @@
+//Product.jsx
 import React from 'react'
 import './Product.css'
+import '../components/Home.css'
 
 export const Product = ({ product, cart, setCart }) => {
 
@@ -12,29 +14,28 @@ export const Product = ({ product, cart, setCart }) => {
 
   return (
  
-        <div className="col-md-3 mb-5">  
-                    <div className="card h-100">
-                        {/* <!-- Product image--> */}
-                        <img className="card-img-top" src={product.image} alt="..." />
-                        {/* <!-- Product details--> */}
-                        <div className="card-body p-4">
-                            <div className="text-center">
-                                {/* <!-- Product name--> */}
-                                <h6 className="fw-bolder">{product.title}</h6>
-                                {/* <!-- Product price--> */}
-                                <br></br>$ {product.price}
-                                <h6>Rating: {product.rating.rate} ({product.rating.count})</h6>
-                            </div>
-                        </div>
-                        {/* <!-- Product actions--> */}
-                        <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div className="text-center">
-                                {cart.includes(product) ? (<a className="btn btn-outline-dark mt-auto" onClick={removeCart} href="#">Remove from Cart</a>) : (<a className="btn btn-outline-dark mt-auto" onClick={addCart} href="#">Add to Cart</a>)}
-                            </div>
+        <div className="product-card">
+                <div className="card h-100">
+                    {/* <!-- Product image--> */}
+                    <img className="card-img-top Image" src={product.image} alt="Product Image"/>
+                    {/* <!-- Product details--> */}
+                    <div className="card-body p-4">
+                        <div className="text-center">
+                            {/* <!-- Product name--> */}
+                            <h6 className="fw-bolder">{product.title}</h6>
+                            {/* <!-- Product price--> */}
+                            <br></br>$ {product.price}
+                            <p>Rating: {product.rating.rate} ({product.rating.count})</p>
                         </div>
                     </div>
-
-         </div>
+                    {/* <!-- Product actions--> */}
+                    <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                        <div className="text-center">
+                            {cart.includes(product) ? (<a className="btn btn-outline-dark mt-auto" onClick={removeCart} href="#">Remove from Cart</a>) : (<a className="btn btn-outline-dark mt-auto" onClick={addCart} href="#">Add to Cart</a>)}
+                        </div>
+                    </div>
+                </div>
+            </div>
  
   )
 }
